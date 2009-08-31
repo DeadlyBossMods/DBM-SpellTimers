8df0e27a3453b032d738fd89b59cb086b7c0b8d9
1nn7erplay <1nn7erplay@d58e4253-be05-4fa4-9d07-250c4b2908de>
2009-08-31 12:35:40 +0000

    Spanish localizations for SpellTimers

---
+++ DBM-SpellTimers/localization.es.lua
@@ -0,0 +1,21 @@
+if GetLocale() ~= "esES" then return end
+local L = DBM_SpellsUsed_Translations
+
+L.TabCategory_SpellsUsed	= "Cd de Echizo/Habilidad"
+L.AreaGeneral 			= "Opciones Generales"
+L.Enable 			= "Habilitar cdsEnable cooldown timers"
+L.Show_LocalMessage 		= "Mostrar mensaje local cuando casteas"
+L.Enable_inRaid			= "Mostrar cds solo de miembros de la raid"
+L.Enable_inBattleground		= "Mostrar cds en batallas"
+L.Enable_Portals		= "Mostrar duracion de portales"
+L.Reset				= "Reinciar de fabrica"
+L.Local_CastMessage 		= "Cast detectado: %s"
+L.AreaAuras 			= "Setup spells/skills"
+L.SpellID 			= "ID de Hechizo"
+L.BarText 			= "Bar Text (default: %spell: %player)"
+L.Cooldown 			= "Cooldown"
+L.Error_FillUp			= "Please fill all fields before adding a new one"
+
+
+
+
