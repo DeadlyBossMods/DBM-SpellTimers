bbd8c7fea6aec4f709a5bae4e69b587af34542e7
Snamor <Snamor@d58e4253-be05-4fa4-9d07-250c4b2908de>
2010-01-29 12:16:23 +0000

    esES Spelltimers Update

--- DBM-SpellTimers/localization.es.lua
+++ DBM-SpellTimers/localization.es.lua
@@ -1,6 +1,8 @@
 if GetLocale() ~= "esES" then return end
 local L = DBM_SpellsUsed_Translations
 
+
+
 L.TabCategory_SpellsUsed	= "Cd de Echizo/Habilidad"
 L.AreaGeneral 			= "Opciones Generales"
 L.Enable 			= "Habilitar cdsEnable cooldown timers"
@@ -10,11 +12,11 @@ L.Enable_inBattleground		= "Mostrar cds en batallas"
 L.Enable_Portals		= "Mostrar duracion de portales"
 L.Reset				= "Reinciar de fabrica"
 L.Local_CastMessage 		= "Cast detectado: %s"
-L.AreaAuras 			= "Setup spells/skills"
+L.AreaAuras 			= "Configurar hechizos/habilidades"
 L.SpellID 			= "ID de Hechizo"
-L.BarText 			= "Bar Text (default: %spell: %player)"
+L.BarText 			= "Texto de barra (defecto: %spell: %player)"
 L.Cooldown 			= "Cooldown"
-L.Error_FillUp			= "Please fill all fields before adding a new one"
+L.Error_FillUp			= "Por favor, rellena los campos antes de añadir otro"
 
 
 
