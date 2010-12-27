1b50a3f95e0475e5bd16173bff28c8699b3f1df6
sasmira <sasmira@d58e4253-be05-4fa4-9d07-250c4b2908de>
2010-12-27 08:38:24 +0000

    french translation updated

--- trunk/DBM-SpellTimers/localization.fr.lua
+++ trunk/DBM-SpellTimers/localization.fr.lua
@@ -1,6 +1,8 @@
--- ckeurk(http://www.deadlybossmods.com/phpbb3/viewtopic.php?f=5&t=2225)
 if GetLocale() ~= "frFR" then return end
 local L = DBM_SpellsUsed_Translations
+
+-- Initial translation by ckeurk
+-- Last update: 12/27/2010 (by Sasmira)
 
 L.TabCategory_SpellsUsed	= "incantation/Capacit\195\169 temps de recharge"
 L.AreaGeneral        		= "Options générales"
@@ -12,7 +14,7 @@ L.Enable_Portals			= "Dur\195\169es des portails"
 L.Reset						= "Remettre \195\160 z\195\169ro"
 L.Local_CastMessage			= "D\195\169tecte les incant: %s"
 L.AreaAuras					= "Syst\195\168me des sorts / compétences"
-L.SpellID					= "Sortilège ID"
+L.SpellID					= "ID du sort"
 L.BarText					= "Barre de texte (par défaut: %sort: %joueur)"
 L.Cooldown					= "Temps de recharge"
 L.Error_FillUp				= "merci de remplir tous les champs avant d'ajouter un nouveau"
