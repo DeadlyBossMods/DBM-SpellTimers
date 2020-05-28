27328af3a94fa95a58eed2a68b24793875b546b2
QartemisT <QartemisT@gmail.com>
2020-05-28 01:26:26 +0100

    Fix locales

--- DBM-SpellTimers/localization.es.lua
+++ DBM-SpellTimers/localization.es.lua
@@ -1,19 +1,26 @@
 if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then
 	return
 end
-local L = DBM_SpellsUsed_Translations
+local L = DBM:GetModLocalization("SpellTimers")
 
-L.TabCategory_SpellsUsed	= "Tiempos de reutilización"
-L.AreaGeneral 				= "Opciones generales"
-L.Enable 					= "Habilitar temporizadores"
-L.Show_LocalMessage 		= "Enviar mensaje con el lanzamiento"
-L.Enable_inRaid				= "Mostrar solo tiempos de reutilización de los miembros de banda"
-L.Enable_inBattleground		= "Mostrar tiempos de reutilización en campos de batalla"
-L.Enable_Portals			= "Mostrar duración de portales"
-L.Reset						= "Restaurar"
-L.Local_CastMessage 		= "Lanzamiento detectado: %s"
-L.AreaAuras 				= "Configuración de hechizos y facultades"
-L.SpellID 					= "ID"
-L.BarText 					= "Texto"
-L.Cooldown 					= "Tiempo de reutilización"
-L.Error_FillUp				= "Por favor, rellena todos los campos antes de añadir otro hechizo o facultad."
+L:SetGeneralLocalization({
+	name	= "SpellTimers"
+})
+
+L:SetOptionLocalization({
+	Enable 					= "Habilitar temporizadores",
+	Show_LocalMessage 		= "Enviar mensaje con el lanzamiento",
+	Enable_inRaid			= "Mostrar solo tiempos de reutilización de los miembros de banda",
+	Enable_inBattleground	= "Mostrar tiempos de reutilización en campos de batalla",
+	Enable_Portals			= "Mostrar duración de portales",
+})
+
+L:SetMiscLocalization({
+	Local_CastMessage 		= "Lanzamiento detectado: %s",
+	AreaAuras 				= "Configuración de hechizos y facultades",
+	SpellID 				= "ID",
+	BarText 				= "Texto",
+	Cooldown 				= "Tiempo de reutilización",
+	Error_FillUp			= "Por favor, rellena todos los campos antes de añadir otro hechizo o facultad.",
+	TabCategory_SpellsUsed	= "Tiempos de reutilización"
+})
