253ec62bd131dd51f89db90bbf01ea9abde20f69
MysticalOS <mysticalosx@gmail.com>
2026-09-20 15:32:25 -0500

    tweaks

--- DBM-SpellTimers/SpellTimers.lua
+++ DBM-SpellTimers/SpellTimers.lua
@@ -26,7 +26,7 @@
 --    * Noncommercial. You may not use this work for commercial purposes.
 --    * Share Alike. If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
 --
-local isClassic = DBM:IsVanillaEra()
+local isClassic = DBM:IsVanilla()
 local isTBC = DBM:IsTBC()
 
 local default_bartext = "%spell: %player"
